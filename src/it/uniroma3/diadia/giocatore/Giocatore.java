d9f859f1c2ad3dc9ef92016a131aac463fcbc451
hccsan <72209607+hccsan@users.noreply.github.com>
2023-03-29 14:24:03 +0200

    Versione hw1

---
+++ src/it/uniroma3/diadia/giocatore/Giocatore.java
@@ -0,0 +1,32 @@
+package it.uniroma3.diadia.giocatore;
+public class Giocatore {
+	
+	static final private int CFU_INIZIALI = 20;
+	
+	private Borsa borsa;
+	private int cfu;
+	
+	public Giocatore() {
+		this.cfu = CFU_INIZIALI;
+		this.borsa = new Borsa();
+	}
+
+	public Borsa getBorsa() {
+		return borsa;
+	}
+
+	public void setBorsa(Borsa borsa) {
+		this.borsa = borsa;
+	}
+
+	public int getCfu() {
+		return cfu;
+	}
+
+	public void setCfu(int cfu) {
+		this.cfu = cfu;
+	}
+	
+	
+
+}
